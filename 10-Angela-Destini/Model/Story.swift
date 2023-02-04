f3bcbda53159970d2b363026f4a46edd90a14e28
Aleksei <aleksei@mail.ru>
2023-02-04 22:00:41 +0300

    Complete

---
+++ 10-Angela-Destini/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  10-Angela-Destini
+//
+//  Created by Алексей on 04.02.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
